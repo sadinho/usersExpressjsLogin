9f7f1daa6cd772172ecd449aaed936c2e2de8b19
sadinho <50503203+sadinho@users.noreply.github.com>
2021-09-15 22:20:29 -0300

    ajuste

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use(router);
 
-app.listen(3001, () => console.log('server started localhost:3001'));
+app.listen(8000, () => console.log('server started localhost:8000'));
